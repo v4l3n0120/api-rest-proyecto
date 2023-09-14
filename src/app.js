84a89ee2b7a1671c304e5138cd2068dc83a7ea81
mileycahi2003 <achimaleydi02@gmail.com>
2023-09-14 17:18:20 -0500

    agregar cambio en app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import express from "express"
-import employeesRoutes from "./routes/employees.routes.js"
-import indexRoutes from "./routes/index.routes.js"
+import compradoresRoutes from "./routes/compradores.routes.js"
+import facturaRoutes from "./routes/factura.routes.js"
+import productosRoutes from "./routes/productos.routes.js"
+import vendedoresRoutes from "./routes/vendedores.routes.js"
 
 const app = express()
 
 app.use(express.json())
 
-app.use(indexRoutes)
-app.use("/api",employeesRoutes)
+
+app.use("/api",compradoresRoutes)
+app.use("/api",facturaRoutes)
+app.use("/api",productosRoutes)
+app.use("/api",vendedoresRoutes)
+
+
 
 app.use((req, res, next)=>{
     res.status(404).json({
